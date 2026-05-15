8ad9e84ada47ac9fda6c1711ac1fddedee0fc9ce
AndreiTica <cikamide665@gmail.com>
2026-05-15 12:02:27 +0300

    Creare proiect SpringBoot

---
+++ BackEnd/genomics-api/src/main/java/ro/licenta/genomicsapi/GenomicsApiApplication.java
@@ -0,0 +1,13 @@
+package ro.licenta.genomicsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenomicsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenomicsApiApplication.class, args);
+    }
+
+}
---
+++ BackEnd/genomics-api/src/test/java/ro/licenta/genomicsapi/GenomicsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.licenta.genomicsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenomicsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
